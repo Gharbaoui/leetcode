0647b00e890d550eeb5a5febcfa6b864fcdf57b9
gharbaoui <mohamed9gharbaoui@gmail.com>
2023-08-12 15:51:31 +0100

    done implementing hash map

--- implementation/hash_map/src/hash_map.hpp
+++ implementation/hash_map/src/hash_map.hpp
@@ -1,8 +1,88 @@
 #pragma once
+#include <utility>
+#include <array>
+#include <list>
+#include <cstdint>
+#include <type_traits>
+#include <algorithm>
+#include <optional>
+
+#ifndef TESTING
+#define TESTING
+#endif
+
+template <typename Func, typename Arg>
+concept CanBeHashed = requires(Func f, Arg key) {
+    {f(key)} -> std::convertible_to<std::uint16_t>;
+}; // NOLINT
 
 template <
     typename Key,
     typename T,
-    typename Hash>
+    typename Hash,
+    std::uint16_t ArrSize>
+requires CanBeHashed<Hash, Key>
 class   HashMap {
+ public:
+    using ObjectType = std::pair<Key, T>;
+    using ListOfObjects = std::list<ObjectType>;
+    explicit HashMap(Hash& hf): hashFunction(hf) {} // NOLINT
+
+    bool    insert(const ObjectType obj) {
+         const std::uint16_t index = hashFunction(obj.first);
+         auto& listOfObjs = hashMapArr[index];
+        if (objectInList(index, obj.first)) {
+            // object already stored
+            return false;
+        }
+         listOfObjs.push_front(obj);
+         return true;
+    }
+
+    std::pair<bool, std::optional<T>>    search(const Key& key) const {
+         const std::uint16_t index = hashFunction(key);
+         const auto& listOfObjs = hashMapArr[index];
+         const auto& it = std::find_if(listOfObjs.begin(), listOfObjs.end(),
+                 [&](const ObjectType& obj) {return obj.first == key;});
+         const bool objectWasFound = (it != listOfObjs.end());
+         if (objectWasFound) {
+            return std::make_pair(
+                    true,
+                    std::make_optional(it->second));
+         }
+        return std::make_pair(
+            false,
+            std::optional<T>());
+    }
+
+    bool    erase(const Key& key) {
+         const std::uint16_t index = hashFunction(key);
+         auto& listOfObjs = hashMapArr[index];
+         const auto& it = std::find_if(listOfObjs.begin(), listOfObjs.end(),
+                 [&](const ObjectType& obj) {return obj.first == key;});
+         const bool objectWasFound = (it != listOfObjs.end());
+         if (objectWasFound) {
+            listOfObjs.erase(it);
+            return true;
+         }
+         return false;
+    }
+
+ private:
+    bool    objectInList(std::uint16_t listIndex, const Key& key) const {
+         const auto& listOfObjs = hashMapArr[listIndex];
+         const auto& it = std::find_if(listOfObjs.begin(), listOfObjs.end(),
+                 [&](const ObjectType& obj) {return obj.first == key;});
+         return (it != listOfObjs.end());
+    }
+
+ private:
+    Hash&  hashFunction;
+    std::array<ListOfObjects, ArrSize> hashMapArr;
+    #ifdef TESTING
+    FRIEND_TEST(HashMapInsert, SizeChange);
+    FRIEND_TEST(HashMapInsert, ValueCheck);
+    FRIEND_TEST(HashMapInsert, ValueAlreadyStored);
+    FRIEND_TEST(HashMapSearch, NonEmptyHashMap);
+    #endif
 };
--- implementation/hash_map/tests/hash_map_test.cpp
+++ implementation/hash_map/tests/hash_map_test.cpp
@@ -1,6 +1,116 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include "../src/hash_map.hpp"
+#include <utility>
+#include <string>
 
-TEST(TC, TS) {
-	EXPECT_TRUE(false);
+TEST(HashMapInsert, SizeChange) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    EXPECT_EQ(hm.hashMapArr[2].size(), 0);
+
+    const bool ret = hm.insert({3, "Hello"});
+
+    EXPECT_EQ(hm.hashMapArr[2].size(), 1);
+    EXPECT_TRUE(ret);
+}
+
+TEST(HashMapInsert, ValueCheck) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    const bool ret = hm.insert({3, "Hello"});
+
+    EXPECT_EQ(hm.hashMapArr[2].front(), std::make_pair(3, std::string("Hello")));
+    EXPECT_TRUE(ret);
+}
+
+TEST(HashMapInsert,ValueAlreadyStored) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    EXPECT_EQ(hm.hashMapArr[2].size(), 0);
+
+    bool ret = hm.insert({3, "Hello"});
+
+    EXPECT_EQ(hm.hashMapArr[2].size(), 1);
+    EXPECT_TRUE(ret);
+
+    ret = hm.insert({3, "world"});
+    EXPECT_EQ(hm.hashMapArr[2].size(), 1);
+    EXPECT_EQ(hm.hashMapArr[2].front(), std::make_pair(3, std::string("Hello")));
+    EXPECT_FALSE(ret);
+}
+
+TEST(HashMapSearch, EmptyHashMap) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    EXPECT_FALSE(
+        hm.search(3).first
+    );
+}
+
+TEST(HashMapSearch, NonEmptyHashMap) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    hm.hashMapArr[2].push_back({9, "owner"});
+
+    const auto& found = hm.search(9);
+
+    EXPECT_TRUE(
+        found.first
+    );
+    
+    EXPECT_EQ(
+            found.second,
+            std::string("owner")
+    );
+}
+
+TEST(HashMapErase, ValueDoesNotExist) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    EXPECT_FALSE(
+        hm.erase(3)
+    );
+}
+
+TEST(HashMapErase, ValueDoesExist) {
+    const auto HashFunction = [](int) {
+        return 2;
+    };
+
+    HashMap<int, std::string, decltype(HashFunction), 4> hm (HashFunction);
+
+    hm.insert({9, "car"});
+
+    EXPECT_TRUE(
+        hm.erase(9)
+    );
+
+    const auto& found = hm.search(9);
+
+    EXPECT_FALSE(found.first);
 }
